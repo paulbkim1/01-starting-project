07db9e99112d0970cd87ddd271cf3c6b6b53c8e2
paulbkim1 <paulbkim1@gmail.com>
2023-12-22 14:45:43 -0800

    first commit

---
+++ src/hooks/useInput.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+export default function useInput(defaultValue, validationFn) {
+  const [enteredValue, setEnteredValue] = useState(defaultValue);
+  const [didEdit, setDidEdit] = useState(false);
+
+  const valueIsvalid = validationFn(enteredValue);
+
+  function handleInputChange(event) {
+    setEnteredValue(event.target.value);
+    setDidEdit(false);
+  }
+
+  function handleInputBlur() {
+    setDidEdit(true);
+  }
+
+  return {
+    value: enteredValue,
+    handleInputChange,
+    handleInputBlur,
+    hasError: didEdit && !valueIsvalid,
+  };
+}
